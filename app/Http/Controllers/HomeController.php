6ba69c1d3ed947c3e9b543af0cc05868544612eb
Alga Saputra <68761934+AlgaSaputra@users.noreply.github.com>
2021-07-22 21:49:16 +0700

    Update HomeController.php

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -26,6 +26,7 @@ class HomeController extends Controller
     {
         $keyword = $request->keyword;
         $conten = Conten::where('pekerjaan', 'LIKE', '%'.$keyword.'%')
+        ->orwhere('nama_perusahaan', 'LIKE', '%'.$keyword.'%')
         ->paginate(10);
         $conten->appends($request->all());
         return view('user.content', compact('conten'));
